adff5a09aebb83777bf79a0e96e478a1c3f41187
Paolo Abadesco <pjabadesco@gmail.com>
2013-09-08 20:28:25 +0800

    3

--- www/pdfjs/web/viewer.js
+++ www/pdfjs/web/viewer.js
@@ -21,7 +21,7 @@
 
 'use strict';
 
-var DEFAULT_URL = 'compressed.tracemonkey-pldi-09.pdf';
+var DEFAULT_URL = 'file:///storage/sdcard0/theFile.pdf';
 var DEFAULT_SCALE = 'auto';
 var DEFAULT_SCALE_DELTA = 1.1;
 var UNKNOWN_SCALE = 0;
